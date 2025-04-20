53570ed0aba0874851edd3a9cbbb53bce99db0c5
Lakshman Siva <slakshman664@gmail.com>
2025-04-20 18:59:52 -0400

    Added the ShadcnUI and using their theme module

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from './components/theme-provider'
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,11 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-white dark:bg-gray-900 text-black dark:text-white transition-colors duration-300 antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,8 @@
 // app/page.tsx
-import LeftPanel from './components/LeftPanel'
-import RightPanel from './components/RightPanel'
-import ThemeToggle from './components/ThemeToggle'
 
 export default function Home() {
   return (
-    <div className="flex h-screen overflow-hidden">
-      <div className="w-1/4 bg-gray-100 dark:bg-gray-800 p-4">
-        <ThemeToggle />
-        <LeftPanel />
-      </div>
-      <div className="flex-1 bg-gray-50 dark:bg-gray-900 p-4">
-        <RightPanel />
-      </div>
-    </div>
+    <>
+    </>
   )
 }
